3135e3478aec47090181b2cddf33803d6e81da86
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-11-15 11:23:52 +0530

    Add files via upload

---
+++ Readmi.md
@@ -0,0 +1,8 @@
+to upload the file:- http://localhost:4000/api/upload    key will be file
+
+get the uploaded file:- http://localhost:4000/api/file/Ishihara_Tests.pdf
+
+
+page update:- http://localhost:4000/api/pdf/extract/Ishihara_Tests.pdf/3/4
+
+created new pdf: http://localhost:4000/api/file/Ishihara_Tests.pdf_extracted.pdf
---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fileRoutes = require('./routes/fileRoutes');
+const pdfRoutes = require('./routes/pdfRoutes');
+
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use('/api', fileRoutes);
+app.use('/api/pdf', pdfRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
