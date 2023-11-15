f69bd7238e9a5e41376321d27ddaa18aef50c237
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-11-15 11:23:00 +0530

    Delete Backend directory

--- Backend/routes/pdfRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { extractPages } = require('../controllers/pdfController');
-
-const router = express.Router();
-
-router.get('/extract/:filename/:startPage/:endPage', extractPages);
-
-module.exports = router;
